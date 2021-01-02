0f8914c6c1c33abe3add695b15760ec3dfed5f68
Caleb Schoepp <caleb.schoepp@gmail.com>
2021-01-02 01:03:04 -0700

    Center title

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import Help from "./Help";
 function App() {
   return (
     <div className="absolute bottom-0 top-0 left-0 right-0">
-      <h1 className="absolute top-0 p-2 text-2xl mx-auto">
+      <h1 className="absolute top-0 p-2 text-2xl w-full text-center">
         Spotify Queue Shuffler
       </h1>
       <Shuffler />
